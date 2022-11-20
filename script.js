6849c2b8936df90e708d63086301874b30ea25e2
lightwingfire <endr.inclightwing@gmail.com>
2022-11-20 14:48:16 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let background = 0;
+
+function mouth() {
+    alert("You clicked the 'mouth'");
+  }
+
+function changeBackground(){
+    background = background + 1;
+    if (background > 10) {
+        background = 0;
+    }
+    if (background == 0){
+        document.body.style.background = "white";
+    } else if (background == 1){
+        document.body.style.background = "black";
+    } else if (background == 2){
+        document.body.style.background = "red";
+    } else if (background == 3){
+        document.body.style.background = "orange";
+    }else if (background == 4){
+        document.body.style.background = "yellow";
+    }else if (background == 5){
+        document.body.style.background = "green";
+    }else if (background == 6){
+        document.body.style.background = "blue";
+    } else if (background == 7){
+        document.body.style.background = "indigo";
+    } else if (background == 8){
+        document.body.style.background = "violet";
+    }
+    
+}
