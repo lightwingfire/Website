f2350d5e62f0f34aa7d35b6672e4c3775af6374a
lightwingfire <endr.inclightwing@gmail.com>
2022-11-21 15:15:26 -0500

    eyes

--- script.js
+++ script.js
@@ -11,9 +11,6 @@ function mouth() {
     }
     
   }
-function mouth2(){
-    
-}
 
 function changeBackground(){
     background = background + 1;
@@ -40,4 +37,29 @@ function changeBackground(){
         document.body.style.background = "violet";
     }
     
+}
+
+var eyeBall = document.querySelector('#eyeBall');
+var eyeBall2 = document.querySelector('#eyeBall2');
+
+document.onmousemove = (event) => {
+    var x = event.clientX * 100 / window.innerWidth + "%";
+    var y = event.clientY * 100 / window.innerHeight + "%";
+    
+    eyeBall.style.transition = "0s";
+    eyeBall.style.left = x;
+    eyeBall.style.top = y;
+
+    eyeBall2.style.transition = "0s";
+    eyeBall2.style.left = x;
+    eyeBall2.style.top = y;
+}
+
+document.onmouseout = (event) => {
+    eyeBall.style.transition = "0.3s";
+    eyeBall.style.left = "50%";
+    eyeBall.style.top = "50%";
+    eyeBall2.style.transition = "0.7s";
+    eyeBall2.style.left = "50%";
+    eyeBall2.style.top = "50%";
 }
